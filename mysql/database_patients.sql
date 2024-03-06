0bc9ede1265ae9ff7146886bb1a72b7f2b50bf87
Oscar Li <oli12myseneca.ca>
2024-03-06 16:52:27 -0500

    fix

--- mysql/database_patients.sql
+++ mysql/database_patients.sql
@@ -1,11 +1,23 @@
 use Hospital;
 
 CREATE TABLE patients(
-    PatientID int not null AUTO_INCREMENT,
-    FirstName VARCHAR(100) NOT NULL,
-    Surname VARCHAR(100) NOT NULL,
-    PRIMARY KEY (PatientID)
+  PatientID int not null AUTO_INCREMENT,
+  ClinicNumber int unsigned NOT NULL,
+  Birthday date DEFAULT NULL,
+  FirstName varchar(100) DEFAULT NULL,
+  Surname varchar(100) DEFAULT NULL,
+  Email varchar(100) DEFAULT NULL,
+  PRIMARY KEY (PatientID),
+  UNIQUE KEY PatientID_UNIQUE (PatientID),
+  UNIQUE KEY ClinicNumber_UNIQUE (ClinicNumber)
 );
 
-INSERT INTO patients(FirstName, Surname)
-VALUES("John", "Anderson"), ("Emma", "Smith"), ("Oscar", "Li"), ("John", "Shi"), ("Jacky", "Li"), ("DoubleUTF", "Li");
+INSERT INTO patients
+(ClinicNumber,Birthday,FirstName,Surname,Email)
+VALUES
+(123456789,'1990-10-29','John','Anderson','JohnAnderson@gmail.com'),
+(123556789, '1993-01-21','Emma','Smith','EmmaSmith@gmail.com'),
+(123456389,'1998-03-18','Oscar','Li','OscarLi@gmail.com'),
+(123456289,'1996-05-13','Johnny','Shi','JohnnyShi@gmail.com'),
+(123456489, '2000-17-1', 'James', 'Mansor', 'JamesMansor@gmail.com');
+
